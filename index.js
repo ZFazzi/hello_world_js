fcfef36cbd30b431f4f86c237135ea4efb71f89b
Zakia Fazzi <kia.fazzi@gmail.com>
2024-10-15 22:10:33 +0200

    added the some content to the console

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+alert('Hello World!');
+
+let name = "Zakia";  
+let age = 31;  
+let favoriteColor = "Yellow";
+
+console.log('My name is ' + name,', my age is ' + age, 'and my favorite color is ' + favoriteColor); 
+
+
+
+let num1 = 8;
+let num2 = 6;
+
+console.log('My numbers are ' + num1 + ' and ' + num2);
+ 
+console.log('addition: ' + (num1 + num2));
+console.log('subtraction: ' + (num1 - num2));
+console.log('multiplication: ' + (num1 * num2));
+console.log('division: ' + (num1 / num2));
