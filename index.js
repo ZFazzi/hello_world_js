04870c21a9268b7c9a513f22190be7267b8a4e7c
Zakia Fazzi <kia.fazzi@gmail.com>
2024-10-20 22:15:17 +0200

    calculation for discount price

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-console.log('Hello World!');
+// Ask the user to enter a price tag using prompt(), for example: 49.99
+let price = prompt("Enter the price, for example: 49.99");
+
+// Convert the input from string to number
+price = Number(price);
+
+// Print out the chosen price
+console.log(`Chosen price: $${price}`);
+
+// Reduce the price by 10 percent
+let discountPrice = price - (price * 0.10);
+
+// Print the calculation of discount price
+console.log(`Discount: ${price} - (${price} * 0.10) = ${discountPrice}`);
+
+// Print out the new price
+console.log(`New price: $${discountPrice}`);
+
+
+
+
+/*Remembered I shouldnt use backticks and dollarsigns so ill comment this out
+
+//Ask the user to enter a price tag using prompt(), for example: $49.99
+let price = prompt("Enter the price, for example: 49.99");
+// Convert the input from string to number
+price = Number(price);
+//print out the chosen price
+console.log("Chosen price: $" + price);
+//Reduce the price by 10 procent
+let discountPrice = price - (price * 0.10);
+//print the calculation of discount price
+console.log("Discount: " + price + " - (" + price + " * 0.10) = " + (price - price * 0.10));
+// Print out the new price using console.log(), for example: 'New price: $44.99'
+console.log("New price: $" + discountPrice);
+*/
+
+
+
+
+//First assignment
+/*console.log('Hello World!');
 alert('Hello World!');
 
 let name = "Zakia";  
@@ -15,4 +56,4 @@ console.log('My numbers are ' + num1 + ' and ' + num2);
 console.log('addition: ' + (num1 + num2));
 console.log('subtraction: ' + (num1 - num2));
 console.log('multiplication: ' + (num1 * num2));
-console.log('division: ' + (num1 / num2));
+console.log('division: ' + (num1 / num2)); */
