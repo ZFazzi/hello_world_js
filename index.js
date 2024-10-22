9edbd5bcfec31ab31f9ac009c4cca21940574c49
Zakia Fazzi <kia.fazzi@gmail.com>
2024-10-22 23:23:32 +0200

    Assigment 3 - functions added

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+//Assigment 3
+function isValidPassword(password, username) {
+    //Check if password is atleast 8 characters long
+    if (password.length < 8) {
+        return false;
+    }
+    //Check if password contains spaces
+    if (password.includes(' ')) {
+        return false;
+    }
+    //Check if password contains the username
+    if (password.includes(username)) {
+        return false;
+    }
+    //If all conditions are met, return true
+    else{
+    return true;
+    }
+}
+
+// Examples
+console.log(isValidPassword('password123', 'Anna'));  // true
+console.log(isValidPassword('pass word', 'Ahmed'));    // false
+console.log(isValidPassword('pass', 'Daphne'));     // false
+console.log(isValidPassword('Markopolo', 'Marko'));     // false
+
+
+
+/*Second assignment
 
 // Ask the user to enter a price tag using prompt(), for example: 49.99
 let price = prompt("Enter the price, for example: 49.99");
@@ -17,30 +46,15 @@ console.log(`Discount: ${price} - (${price} * 0.10) = ${discountPrice}`);
 // Print out the new price
 console.log(`New price: $${discountPrice}`);
 
-
-
-
-/*Remembered I shouldnt use backticks and dollarsigns so ill comment this out
-
-//Ask the user to enter a price tag using prompt(), for example: $49.99
-let price = prompt("Enter the price, for example: 49.99");
-// Convert the input from string to number
-price = Number(price);
-//print out the chosen price
-console.log("Chosen price: $" + price);
-//Reduce the price by 10 procent
-let discountPrice = price - (price * 0.10);
-//print the calculation of discount price
-console.log("Discount: " + price + " - (" + price + " * 0.10) = " + (price - price * 0.10));
-// Print out the new price using console.log(), for example: 'New price: $44.99'
-console.log("New price: $" + discountPrice);
 */
 
 
 
 
-//First assignment
-/*console.log('Hello World!');
+
+/*First assignment
+
+console.log('Hello World!');
 alert('Hello World!');
 
 let name = "Zakia";  
@@ -57,4 +71,6 @@ console.log('My numbers are ' + num1 + ' and ' + num2);
 console.log('addition: ' + (num1 + num2));
 console.log('subtraction: ' + (num1 - num2));
 console.log('multiplication: ' + (num1 * num2));
-console.log('division: ' + (num1 / num2)); */
+console.log('division: ' + (num1 / num2)); 
+
+*/
